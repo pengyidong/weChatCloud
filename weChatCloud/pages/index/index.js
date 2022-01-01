346fc8f92e1ce2ad28bf186fcd09ca3cdba5b8eb
pengyidong <1124666946@qq.com>
2022-01-02 00:50:49 +0800

    引入vant

--- weChatCloud/pages/index/index.js
+++ weChatCloud/pages/index/index.js
@@ -7,38 +7,14 @@ Page({
     list: []
   },
   onLoad() {
-    wx.cloud.database().collection('sales')
-    // .where({region: '华东'})
-    .doc('bf4a0bf261c82cf4009c46236befcac3')
-    .update({
-      data: {
-        city: '江西'
-      }
-    })
-    .then(res => {
-      console.log('res =>', res)
-    })
-    .catch(err => {
-      console.log('err =>', err)
-    })
-    // 获取一个集合
-    // get查询sales集合
-    // where 条件查询
-    // doc 查询单条数据
-    wx.cloud.database().collection('sales')
-      // .where({region: '华东'})
-      // .doc('bf4a0bf261c82cf4009c46236befcac3')
+    const db = wx.cloud.database()
+    db.collection('indexList')
       .get()
       .then(res => {
-        console.log('res =>', res)
-        // this.setData({
-        //   list: res.data
-        // })
+        console.log('res.data', res.data);
+        this.setData({
+          list: res.data
+        })
       })
-      .catch(err => {
-        console.log('err =>', err)
-      })
-
-
-  },
+  }
 })
